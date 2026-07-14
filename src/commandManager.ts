f3bc2dcf8f554ad63d6ed5dee892f211ac5c0c47
h.dasomi <heydar@maxduel.io>
2026-07-14 19:17:03 +0330

    Added 'KEYS' command

--- src/commandManager.ts
+++ src/commandManager.ts
@@ -1,15 +1,19 @@
 import { decode, decodeError } from "./parser";
 import { RedisRepository } from "./repository/redis.repository";
 
+function compareCommand(command0: string, command1: string): Boolean {
+    return command0.toLowerCase() === command1.toLowerCase();
+}
+
 export async function commandManager(command: string[], redisRepository: RedisRepository): Promise<Buffer> {
-    if (command[0] == 'PING') {
+    if (compareCommand(command[0], 'PING')) {
         return Buffer.from("+PONG\r\n")
     }
-    if (command[0] == 'SET') {
+    if (compareCommand(command[0], 'SET')) {
         if (command.includes('px')) {
             const expIn = parseInt(command[command.indexOf("px")+1]);
             if (Number.isNaN(expIn)) {
-                return Buffer.from(decodeError('INVALID_PX'));
+                return Buffer.from(decodeError('ERR value is not an integer or out of range'));
             }
             await redisRepository.setWithExp(
                 command[1],
@@ -22,12 +26,19 @@ export async function commandManager(command: string[], redisRepository: RedisRe
         return Buffer.from("+OK\r\n")
     }
 
-    if (command[0] == 'GET') {
+    if (compareCommand(command[0], 'GET')) {
         const value = await redisRepository.get(command[1]);
         if (value === null) {
             return Buffer.from("$-1\r\n")
         }
         return Buffer.from(decode(value))
     }
+
+    if (compareCommand(command[0], 'KEYS')) {
+        const value = await redisRepository.keys(command[1]);
+        return Buffer.from(decode(value))
+    }
+    
+    return Buffer.from(decodeError(`ERR unknown command \`${command[0]}\``));
 }
 
