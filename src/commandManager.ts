af581fa7c241d6ca3d6d2b6d9d5413e006b9ec0f
h.dasomi <heydar@maxduel.io>
2026-07-15 02:17:35 +0330

    Added 'HGET', 'HSET' and 'HGETALL' command

--- src/commandManager.ts
+++ src/commandManager.ts
@@ -38,6 +38,31 @@ export async function commandManager(command: string[], redisRepository: RedisRe
         const value = await redisRepository.keys(command[1]);
         return Buffer.from(decode(value))
     }
+
+    if (compareCommand(command[0], 'HGET')) {
+        const value = await redisRepository.hget(command[1], command[2]);
+        return Buffer.from(decode(value))
+    }
+
+    if (compareCommand(command[0], 'HSET')) {
+        const hkeys: string[] = [];
+        const hvalues: string[] = [];
+
+        for (let i = 2; i < command.length; i += 2) {
+            hkeys.push(command[i]);
+            hvalues.push(command[i + 1]);
+        }
+        await redisRepository.hset(command[1], hkeys, hvalues);
+        return Buffer.from("+OK\r\n")
+    }
+
+    if (compareCommand(command[0], 'HGETALL')) {
+        const result = await redisRepository.hgetall(command[1]);
+        if (result === null) {
+            return Buffer.from("$-1\r\n")
+        }
+        return Buffer.from(decode(Object.entries(result).flat()))
+    }
     
     return Buffer.from(decodeError(`ERR unknown command \`${command[0]}\``));
 }
